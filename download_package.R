8247dd9135390e5ae078b5eca739c5ce7360dc09
Bryce Mecum <petridish@gmail.com>
2017-12-15 11:39:14 -0900

    Simplify example in docs
    Two things:
    
    - I wasn't sure why you called this variable mnReal so I just called it mn
    - It's more convential when providing function arguments in their proper order to not specify the argument name

--- download_package.R
+++ download_package.R
@@ -40,8 +40,8 @@ get_package_size <- function(node, package_identifier, formatType = "*") {
 #' @example
 #' \dontrun{
 #' cn <- CNode("PROD")
-#' mnReal <- getMNode(cn, "urn:node:ARCTIC")
-#' downloadPackage(mn = mnReal, resource_map_pid = "resource_map_urn:uuid:2b4e4174-4e4b-4a46-8ab0-cc032eda8269")
+#' mn <- getMNode(cn, "urn:node:ARCTIC")
+#' downloadPackage(mn, "resource_map_urn:uuid:2b4e4174-4e4b-4a46-8ab0-cc032eda8269")
 #' }
 #'
 #' @export
